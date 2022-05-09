49c44121861491da4285bd2e449502f632d71ced
amir <torabi46@gmail.com>
2022-05-09 22:52:37 +0430

    modify of height of searchbox

--- src/components/UI/SearchBox.vue
+++ src/components/UI/SearchBox.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="header-search h-[45px] rounded bg-slate-200 flex text-gray-700 justify-start items-center"
+    class="header-search h-[35px] rounded bg-slate-100 flex text-gray-700 justify-start items-center"
   >
     <span class="cursor-pointer mx-2"
       ><i class="text-gray-400 bi bi-search"></i
