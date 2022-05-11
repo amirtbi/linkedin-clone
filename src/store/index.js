fef49896c20da61e10c282be7b56ff818cffe80a
amir <torabi46@gmail.com>
2022-05-11 11:56:17 +0430

    create firebase module

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,17 @@ import { createStore } from "vuex";
 import actions from "./actions.js";
 import mutations from "./mutations.js";
 import getters from "./getters.js";
+// import module
+import fbModule from "./Modules/firebase/index.js";
 
 const store = createStore({
+  modules: {
+    fbModule,
+  },
   state() {
-    return {};
+    return {
+      post: [],
+    };
   },
   actions: actions,
   mutations: mutations,
