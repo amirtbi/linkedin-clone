64ebf5eadc4c77bc66c5f03fcb7efe8e454dfee1
amir <torabi46@gmail.com>
2022-05-14 14:30:03 +0430

    add animation for routers

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <template>
   <!-- header -->
   <the-header></the-header>
-
-  <router-view></router-view>
+  <main class="flex mt-[80px] pb-[2rem]">
+    <router-view v-slot="slotProps">
+      <transition name="slide" mode="out-in">
+        <component :is="slotProps.Component"></component>
+      </transition>
+    </router-view>
+  </main>
 </template>
 
 <script>
@@ -25,5 +30,19 @@ body {
   min-height: 100vh;
   font-family: "Roboto", sans-serif;
   background-color: rgba(240, 238, 238, 0.8);
+}
+.slide-enter-from,
+.sldie-leave-to {
+  opacity: 0;
+  transform: translateX(-100px);
+}
+.slide-enter-active,
+.slide-leave-active {
+  transition: all 0.5s ease-in-out;
+}
+.slide-enter-to,
+.slide-leave-from {
+  opacity: 1;
+  transform: translateX(0px);
 }
 </style>
