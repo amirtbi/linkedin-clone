17fad934fab2564955a14a36bbdc2aa0390fa28b
amir <torabi46@gmail.com>
2022-05-14 13:07:57 +0430

    create popup button for post

---
+++ src/components/layouts/MainContent/DeleteOptionsCard.vue
@@ -0,0 +1,54 @@
+<template>
+  <li
+    v-for="option in options"
+    class="cursor-pointer flex flex-col w-full hover:bg-gray-300 p-[0.75rem]"
+    :key="option.id"
+  >
+    <div class="options flex items-center w-full">
+      <span class="text-gray-600 text-[1rem] mr-2"
+        ><i class="text-[1.25rem]" :class="option.Icon"></i>
+      </span>
+      <div>
+        <h4 class="text-[0.75rem] font-bold font-Roboto">{{ option.title }}</h4>
+        <p class="text-gray-400 text-[0.75rem]" v-if="option.subTitle !== ''">
+          {{ option.subTitle }}
+        </p>
+      </div>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      options: [
+        {
+          id: new Date().toLocaleDateString(),
+          title: "Save",
+          Icon: "bi bi-bookmark",
+          subTitle: "Save for later",
+        },
+        {
+          id: new Date().toLocaleDateString(),
+          title: "Copy link to post",
+          Icon: "bi bi-link-45deg",
+          subTitle: "",
+        },
+        {
+          id: new Date().toLocaleDateString(),
+          title: "Unfollow Alireza",
+          Icon: "bi bi-x-circle-fill",
+          subTitle: "Stay connected but stop seeing post from Alireza",
+        },
+        {
+          id: new Date().toLocaleDateString(),
+          title: "I don't want to see this",
+          Icon: "bi bi-eye-slash-fill",
+          subTitle: "hide or report this post",
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ src/components/layouts/MainContent/PostSharedActor.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="shared-actor__container flex w-full">
+    <div class="posts__image flex max-w-[60px]">
+      <img
+        class="object-cover rounded-[100%]"
+        src="../../../assets/images/person1.jpg"
+        alt="post"
+      />
+    </div>
+    <div class="ml-[0.75rem] posts__info w-[100%]">
+      <!-- name -->
+      <h3 class="text-[1rem] text-black p-0 font-Roboto font-bold">
+        {{ name }}
+      </h3>
+      <!-- description -->
+      <p class="text-[0.7rem] text-zinc-600">{{ description }}</p>
+      <p class="text-[0.7rem] text-zinc-600">1min:edited</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["description", "name"],
+};
+</script>
