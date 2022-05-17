d73804c3fc729313807c16b6a7e7dea4a4e2f75c
amir <torabi46@gmail.com>
2022-05-17 18:47:56 +0430

    define slot

--- src/components/UI/Brand.vue
+++ src/components/UI/Brand.vue
@@ -1,5 +1,8 @@
 <template>
-  <span><i class="text-indigo-700 text-[2rem] bi bi-linkedin"></i></span>
+  <h1 class="mr-1 text-indigo-600 text-[1.75rem] font-Roboto font-bold">
+    <slot name="brandName"></slot>
+  </h1>
+  <span> <i class="text-indigo-600 text-[2rem] bi bi-linkedin"></i></span>
 </template>
 
 <script>
