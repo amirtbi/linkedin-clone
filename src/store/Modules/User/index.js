f997f11891efed466290dc453caa99c2c7f0f6e4
amir <torabi46@gmail.com>
2022-05-23 13:11:54 +0430

    Add upload funcationality to signup page

--- src/store/Modules/User/index.js
+++ src/store/Modules/User/index.js
@@ -1,26 +1,31 @@
 import authService from "../../../services/auth.js";
 import { doc, collection, setDoc } from "firebase/firestore";
-import { db } from "../../../services/firebase.js";
+import { db, storage } from "../../../services/firebase.js";
 
 import AuthService from "../../../services/auth.js";
+import { storageService } from "../../../services/upload.js";
 // global functions
 class localStorageCl {
-  constructor(token = "", email = "") {
+  constructor(token = "", email = "", photoUrl = "") {
     this.token = token;
     this.email = email;
+    this.url = photoUrl;
   }
   addTo() {
     localStorage.setItem("email", this.email);
     localStorage.setItem("token", this.token);
+    localStorage.setItem("photoUrl", this.url);
   }
   delete() {
     localStorage.removeItem("email");
     localStorage.removeItem("token");
+    localStorage.removeItem("photoUrl");
   }
   get loggedInfo() {
     const info = {
       token: localStorage.getItem("token"),
       email: localStorage.getItem("email"),
+      photoUrl: localStorage.getItem("photoUrl"),
     };
     return info;
   }
@@ -31,6 +36,7 @@ export default {
     return {
       token: null,
       email: null,
+      photoUrl: null,
     };
   },
   mutations: {
@@ -38,6 +44,7 @@ export default {
       console.log("mutated ");
       state.token = payload.token;
       state.email = payload.email;
+      state.photoUrl = payload.photoUrl;
     },
   },
   actions: {
@@ -48,11 +55,19 @@ export default {
       console.log("Auto login...");
       context.commit("setLoggedInfo", { token: token, email: email });
     },
-    async storeLoggedInfo(context, payload) {
+    async storeLoggedInfo(_, payload) {
       try {
+        const userInfo = {
+          email: payload.email,
+          fullname: payload.fullname,
+        };
         // DocRef
-        const docRef = collection(db, "users");
-        await setDoc(doc(docRef), payload);
+        // In case  of generating unique id using firebase
+        // const docRef = collection(db, "users", payload.userId);
+        // In case of generation a unique id by firebase
+        // await setDoc(doc(docRef), userInfo);
+        // In case of custom id
+        await setDoc(doc(db, "users", payload.userId), userInfo);
       } catch (error) {
         console.log(error.message);
       }
@@ -99,21 +114,30 @@ export default {
           userId: user.uid,
           email: user.email,
           fullname: payload.get("fullname"),
+          photoUrl: payload.get("photoUrl"),
         };
         // storing data inside local storage
         const StoreHanlder = new localStorageCl(
           loggedUser.token,
-          loggedUser.email
+          loggedUser.email,
+          loggedUser.photoUrl
         );
         StoreHanlder.addTo();
         await context.dispatch("storeLoggedInfo", {
           fullname: payload.get("fullname"),
           email: user.email,
+          userId: user.uid,
         });
         context.commit("setLoggedInfo", {
           token: loggedUser.token,
           email: loggedUser.email,
         });
+        if (payload.get("photoUrl") !== "") {
+          context.dispatch("uploadImage", {
+            file: payload.get("photoUrl"),
+            userId: user.uid,
+          });
+        }
       } else {
         throw new Error("Sign up has faced some issues");
       }
@@ -124,11 +148,21 @@ export default {
       StoreHanlder.delete();
       await new AuthService().logout();
     },
+    uploadImage(_, payload) {
+      new storageService(payload).uploadImage();
+    },
+    async downloadImage(_, payload) {
+      const response = await new storageService(payload).downloadImage();
+      return response;
+    },
   },
 
   getters: {
     isAuthenticated(state) {
       return !!state.token;
+    },
+    profileImage(state) {
+      return state.photoUrl;
     },
   },
 };
