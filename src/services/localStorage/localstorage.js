3ec6ab5c02e83a9130370d837b88e6b8e3a0ece3
amir <torabi46@gmail.com>
2022-05-26 01:23:45 +0430

    remove tokenExpiration

--- src/services/localStorage/localstorage.js
+++ src/services/localStorage/localstorage.js
@@ -4,21 +4,18 @@ export class localStorageCl {
     this.email = dataTransfered.email || "";
     this.url = dataTransfered.photoUrl || "";
     this.fullname = dataTransfered.fullname || "";
-    // this._tokenExpiration = new Date().getTime();
   }
   addTo() {
     localStorage.setItem("email", this.email);
     localStorage.setItem("token", this.token);
     localStorage.setItem("photoUrl", this.url);
     localStorage.setItem("fullname", this.fullname);
-    // localStorage.setItem("tokenExpiration", this._tokenExpiration);
   }
   delete() {
     localStorage.removeItem("email");
     localStorage.removeItem("token");
     localStorage.removeItem("photoUrl");
     localStorage.removeItem("fullname", this.fullname);
-    // localStorage.removeItem("tokenExpiration");
   }
   get loggedInfo() {
     const info = {
